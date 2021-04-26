010b08d3811fd6d43b4be732f7b3445bd892fe0e
Justin Reese <justaddcoffee@gmail.com>
2021-04-26 10:45:21 -0700

    Remove unused import

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import os
 import click
 from kg_idg import download as kg_download
 from kg_idg import transform as kg_transform
-#from project_name.make_holdouts import make_holdouts
 from kg_idg.merge_utils.merge_kg import load_and_merge
 from kg_idg.query import run_query, parse_query_yaml, result_dict_to_tsv
 from kg_idg.transform import DATA_SOURCES
