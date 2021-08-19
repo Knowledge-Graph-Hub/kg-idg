6ab506f2954540cba18fa7c678f2899b4cee19d0
Harry Caufield <j.harry.caufield@gmail.com>
2021-08-19 11:04:10 -0400

    Remove parts for unzips

--- kg_idg/transform_utils/orphanet/orphanet.py
+++ kg_idg/transform_utils/orphanet/orphanet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import csv
-import gzip
 import logging
 import os
 import re
@@ -12,8 +11,7 @@ from typing import Dict, List, Optional
 
 from kg_idg.transform_utils.transform import Transform
 from kg_idg.utils.transform_utils import write_node_edge_item, \
-    get_item_by_priority, ItemInDictNotFound, parse_header, data_to_dict, \
-    unzip_to_tempdir
+    get_item_by_priority, ItemInDictNotFound, parse_header, data_to_dict
 
 """Not operational yet"""
 
@@ -27,12 +25,11 @@ class OrphanetTransform(Transform):
     """
 
     def __init__(self, input_dir: str = None, output_dir: str = None) -> None:
-        source_name = "drug_central"
+        source_name = "orphanet"
         super().__init__(source_name, input_dir, output_dir)  # set some variables
         self.node_header = ['id', 'name', 'category', 'TDL', 'provided_by']
 
     def run(self, nodes_file: str, edges_file: str) -> None:  # type: ignore
-        """A 'passthrough' transform - all we are doing is just moving the downloaded
-        nodes and edges file to the transformed directory
+        """
         """
         self.pass_through(nodes_file=nodes_file, edges_file=edges_file)
