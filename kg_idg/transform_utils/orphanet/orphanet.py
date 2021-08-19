dad5f628baa7c183825ad26798bd84a5f1e6cc40
Harry Caufield <j.harry.caufield@gmail.com>
2021-08-19 10:57:37 -0400

    Setup for Orphanet transformer

---
+++ kg_idg/transform_utils/orphanet/orphanet.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+import gzip
+import logging
+import os
+import re
+import tempfile
+from collections import defaultdict
+
+from typing import Dict, List, Optional
+
+from kg_idg.transform_utils.transform import Transform
+from kg_idg.utils.transform_utils import write_node_edge_item, \
+    get_item_by_priority, ItemInDictNotFound, parse_header, data_to_dict, \
+    unzip_to_tempdir
+
+"""Not operational yet"""
+
+"""
+Ingest Orphanet triples and transform to edge and node lists.
+"""
+
+
+class OrphanetTransform(Transform):
+    """Transform triples in orphanet.nt to edge and node lists.
+    """
+
+    def __init__(self, input_dir: str = None, output_dir: str = None) -> None:
+        source_name = "drug_central"
+        super().__init__(source_name, input_dir, output_dir)  # set some variables
+        self.node_header = ['id', 'name', 'category', 'TDL', 'provided_by']
+
+    def run(self, nodes_file: str, edges_file: str) -> None:  # type: ignore
+        """A 'passthrough' transform - all we are doing is just moving the downloaded
+        nodes and edges file to the transformed directory
+        """
+        self.pass_through(nodes_file=nodes_file, edges_file=edges_file)
