323e40498d334832f7c654e8a00e41d3ee683b38
caufieldjh <j.harry.caufield@gmail.com>
2021-10-21 15:49:21 -0400

    Add taxon to TCRD Gene IDs

--- kg_idg/transform_utils/tcrd/tcrd-ids.py
+++ kg_idg/transform_utils/tcrd/tcrd-ids.py
@@ -1,6 +1,9 @@
 import uuid
 
+#Doing some rough modeling for TCRD categories for now
+
 from biolink_model_pydantic.model import Gene #type: ignore
+from biolink_model_pydantic.model import Attribute #type: ignore
 
 from koza.cli_runner import koza_app #type: ignore
 
@@ -9,7 +12,9 @@ source_name="tcrd-ids"
 row = koza_app.get_row(source_name)
 
 # Entities
-gene = Gene(id='NCBIGene:' + row['NCBI Gene ID'], #Consider other primary ID?
-            name= row['Name'])
+gene = Gene(id='NCBIGene:' + row['NCBI Gene ID'], #Best option for Biolink
+            name= row['Name'],
+            in_taxon= 'NCBITaxon:9606')
 
+# TODO: Include the TDL in 
 koza_app.write(gene)
