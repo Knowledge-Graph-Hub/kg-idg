c41812b5603adc14d3f39cff3f071dd0d0309fa4
caufieldjh <j.harry.caufield@gmail.com>
2021-12-07 14:01:54 -0500

    Expand HPA transform with expression subcell locations

--- kg_idg/transform_utils/hpa/hpa-data.py
+++ kg_idg/transform_utils/hpa/hpa-data.py
@@ -1,16 +1,23 @@
 import uuid
 
-from biolink_model_pydantic.model import Protein #type: ignore
+from biolink_model_pydantic.model import ( #type: ignore
+    Protein,
+    AnatomicalEntity,
+    GeneToExpressionSiteAssociation,
+    Predicate
+)
 
 from koza.cli_runner import koza_app #type: ignore
 
 source_name="hpa-data"
 
 row = koza_app.get_row(source_name)
+go_lookup = koza_app.get_map('go_term_lookup_map')
 
 # Entities
 protein_list = [] # Some entries have multiple protein IDs
 xref_list = []
+subcell_locations = [] # Some entries have multiple subcell locations, or none
 
 xref_types = {"ENSEMBL:":'Ensembl'}
 for xref_type in xref_types:
@@ -21,6 +28,17 @@ for xref_type in xref_types:
     else:
         xref_list.append(xref_type + str(value))
 
+have_location = False
+if str(row["Subcellular location"]) != "":
+    locations = str(row["Subcellular location"]).split(",")
+    for location in locations:
+        location = location.lower()
+        anatomy = AnatomicalEntity(id=go_lookup[location]['id'], # GO term lookup
+                            description=location,
+                            source="Human Protein Atlas")
+        subcell_locations.append(anatomy)
+    have_location = True
+
 for entry in row["Uniprot"].split(", "):
     if entry != "":
         protein = Protein(id='UniProtKB:' + entry,
@@ -28,5 +46,18 @@ for entry in row["Uniprot"].split(", "):
                     xref=xref_list)
         protein_list.append(protein)
 
+# Association
 for entry in protein_list:
-    koza_app.write(protein)
+    if have_location:
+        association = GeneToExpressionSiteAssociation( #This works for Gene OR GeneProduct
+            id="uuid:" + str(uuid.uuid1()),
+            subject=protein.id,
+            predicate=Predicate.expressed_in,
+            object=anatomy.id,
+            relation="RO:0002206", #"expressed in",
+            source = "Human Protein Atlas"
+        )
+        for location in subcell_locations:
+            koza_app.write(protein, association, anatomy)
+    else:
+        koza_app.write(protein)
