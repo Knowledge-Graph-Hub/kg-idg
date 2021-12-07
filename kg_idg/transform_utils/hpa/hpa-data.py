4cde124a048aa94d131b05444c6a06d7a66fea61
caufieldjh <j.harry.caufield@gmail.com>
2021-12-07 13:04:44 -0500

    Update HPA transform

--- kg_idg/transform_utils/hpa/hpa-data.py
+++ kg_idg/transform_utils/hpa/hpa-data.py
@@ -1,6 +1,6 @@
 import uuid
 
-from biolink_model_pydantic.model import Gene #type: ignore
+from biolink_model_pydantic.model import Protein #type: ignore
 
 from koza.cli_runner import koza_app #type: ignore
 
@@ -9,7 +9,24 @@ source_name="hpa-data"
 row = koza_app.get_row(source_name)
 
 # Entities
-gene = Gene(id='ENSEMBL:' + row['Ensembl'],
-            name= row['Gene'])
+protein_list = [] # Some entries have multiple protein IDs
+xref_list = []
 
-koza_app.write(gene)
+xref_types = {"ENSEMBL:":'Ensembl'}
+for xref_type in xref_types:
+    value = row[xref_types[xref_type]]
+    value = value.replace("/","_") #Gotta handle illegal slashes
+    if str(value) == 'None':
+        continue
+    else:
+        xref_list.append(xref_type + str(value))
+
+for entry in row["Uniprot"].split(", "):
+    if entry != "":
+        protein = Protein(id='UniProtKB:' + entry,
+                    source="Human Protein Atlas",
+                    xref=xref_list)
+        protein_list.append(protein)
+
+for entry in protein_list:
+    koza_app.write(protein)
