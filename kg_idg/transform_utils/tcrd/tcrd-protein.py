f1213b58a9e6f9f8fa9b60710478aa114fcf4447
caufieldjh <j.harry.caufield@gmail.com>
2021-12-07 10:46:26 -0500

    tcrd-protein transform ID correction

--- kg_idg/transform_utils/tcrd/tcrd-protein.py
+++ kg_idg/transform_utils/tcrd/tcrd-protein.py
@@ -1,17 +1,25 @@
 import uuid
 
-#Doing some rough modeling for TCRD categories for now
-
-from biolink_model_pydantic.model import Protein #type: ignore
+from biolink_model_pydantic.model import ( #type: ignore
+    Protein,
+    GeneFamily,
+    DrugToGeneAssociation,
+    Predicate,
+    Attribute
+)
 
 from koza.cli_runner import koza_app #type: ignore
+
+# Most TCRD protein info comes from the tcrd-ids transform.
+# This transform ensures all protein IDs are included,
+# and additionally provides protein families.
 
 source_name="tcrd-protein"
 
 row = koza_app.get_row(source_name)
 
 # Entities
-protein = Protein(id='DrugCentral:' + row['id'],
-            name= row['name'])
+protein = Protein(id='UniProtKB:' + row['uniprot'],
+                    source='TCRD')
 
 koza_app.write(protein)
