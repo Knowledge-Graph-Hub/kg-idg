d2c3ab442f793aaed05b9015da682e8e044fe008
caufieldjh <j.harry.caufield@gmail.com>
2021-12-08 15:53:35 -0500

    Reactome transform updates

--- kg_idg/transform_utils/reactome/chebi2reactome.py
+++ kg_idg/transform_utils/reactome/chebi2reactome.py
@@ -10,13 +10,15 @@ from biolink_model_pydantic.model import ( #type: ignore
 from koza.cli_runner import koza_app #type: ignore
 
 source_name="chebi2reactome"
+full_source_name="Reactome"
 
 row = koza_app.get_row(source_name)
 
 # Entities
 chemical = ChemicalEntity(id='CHEBI:' + row['CHEBI_ID'])
 pathway = Pathway(id='REACT:' + row['REACT_PATH_ID'],
-                    description= row['EVENT_NAME'])
+                    description= row['EVENT_NAME'],
+                    source=full_source_name)
 
 # Association
 association = ChemicalToPathwayAssociation(
@@ -25,6 +27,7 @@ association = ChemicalToPathwayAssociation(
     predicate=Predicate.participates_in,
     object=pathway.id,
     relation = koza_app.translation_table.resolve_term("pathway"),
+    source=full_source_name
 )
 
 koza_app.write(chemical, association, pathway)
