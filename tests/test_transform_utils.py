1cabc08e8e29c7ef5c97e4a459329f08b459561b
caufieldjh <j.harry.caufield@gmail.com>
2021-12-09 11:27:19 -0500

    Minor changes to test_transform_utils

--- tests/test_transform_utils.py
+++ tests/test_transform_utils.py
@@ -78,13 +78,14 @@ class TestTransformUtils(TestCase):
     # Koza transforms have hard-coded sources so we skip the transform
     # (in most cases)
     # and instead ensure they don't proceed if source is incorrect.
-    # (Note that these tests will fail if a non-test transform has been run!)
+    # (Note that these tests will fail if raw or transformed data
+    # already exists for the corresponding transform function.)
 
     @mock.patch('koza.cli_runner.transform_source')
     def test_drug_central_transform(self, mock_transform_source):
         t = DrugCentralTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"drug_central")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # Because the raw source doesn't exit yet
             t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         shutil.rmtree(this_output_dir)
@@ -93,7 +94,7 @@ class TestTransformUtils(TestCase):
     def test_reactome_transform(self, mock_transform_source):
         t = ReactomeTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"reactome")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # Because the raw source doesn't exit yet
             t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         self.assertFalse(mock_transform_source.called)
@@ -103,7 +104,7 @@ class TestTransformUtils(TestCase):
     def test_tcrd_transform(self, mock_transform_source):
         t = TCRDTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"tcrd")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # Because the raw source doesn't exit yet
             t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         self.assertFalse(mock_transform_source.called)
@@ -113,8 +114,9 @@ class TestTransformUtils(TestCase):
     def test_hpa_transform(self, mock_transform_source):
         t = ProteinAtlasTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"hpa")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # Because the raw source doesn't exit yet
             t.run()
+        t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         self.assertFalse(mock_transform_source.called)
         shutil.rmtree(this_output_dir)
@@ -123,7 +125,7 @@ class TestTransformUtils(TestCase):
     def test_atc_transform(self, mock_transform_source):
         t = ATCTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"atc")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # Because the raw source doesn't exit yet
             t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         self.assertFalse(mock_transform_source.called)
