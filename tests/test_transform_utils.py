6ebe812060dd47501ba7a7f6a5d348076d6f15a4
caufieldjh <j.harry.caufield@gmail.com>
2021-12-09 22:10:25 -0500

    Edits to test_transform_utils so it doesn't take forever

--- tests/test_transform_utils.py
+++ tests/test_transform_utils.py
@@ -19,12 +19,13 @@ data_raw_path = 'data/raw/'
 download(yaml_file='download.yaml', output_dir=data_raw_path)
 
 # This takes a while because it's essentially a set of 
-# integration tests, and it ingests full source data
+# integration tests, and for Koza ingests it uses full source data
 # rather than snippets to account for edge cases.
 class TestTransformUtils(TestCase):
 
     def setUp(self) -> None:
-        self.input_dir = data_raw_path
+        self.input_dir = 'tests/resources/snippets/'
+        self.raw_path = data_raw_path
         self.output_dir = 'tests/resources/'
 
     @parameterized.expand([
@@ -62,6 +63,7 @@ class TestTransformUtils(TestCase):
         self.assertTrue(os.path.exists(this_output_dir))
         shutil.rmtree(this_output_dir)
     
+    
     def test_orphanet_transform(self):
         t = OrphanetTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"orphanet")
@@ -69,6 +71,7 @@ class TestTransformUtils(TestCase):
         self.assertTrue(os.path.exists(this_output_dir))
         shutil.rmtree(this_output_dir)
 
+    
     def test_omim_transform(self):
         t = OMIMTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"omim")
@@ -76,6 +79,7 @@ class TestTransformUtils(TestCase):
         self.assertTrue(os.path.exists(this_output_dir))
         shutil.rmtree(this_output_dir)
 
+    
     def test_gocams_transform(self):
         t = GOCAMTransform(self.input_dir,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"gocams")
@@ -86,30 +90,32 @@ class TestTransformUtils(TestCase):
     # Koza transforms
 
     # This transform requires a database load
-    # and that is challenging to test, so we mock much of it
-    @mock.patch('kg_idg.utils.sql_utils.process_data_dump')
+    # but we test that elsewhere (in test_sql_utils)
+    @skip
+    @mock.patch('kg_idg.utils.sql_utils.process_data_dump', return_value=False)
     @mock.patch('koza.cli_runner.transform_source')
     def test_drug_central_transform(self, mock_process_data_dump, mock_transform_source):
-        t = DrugCentralTransform(self.input_dir,self.output_dir)
+        t = DrugCentralTransform(self.raw_path,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"drug_central")
         t.run()
         self.assertTrue(os.path.exists(this_output_dir))
-        self.assertTrue(mock_transform_source.called)
         self.assertTrue(mock_process_data_dump.called)
+        self.assertTrue(mock_transform_source.called)
         shutil.rmtree(this_output_dir)
 
     def test_reactome_transform(self):
-        t = ReactomeTransform(self.input_dir,self.output_dir)
+        t = ReactomeTransform(self.raw_path,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"reactome")
         t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         shutil.rmtree(this_output_dir)
 
     # Another database load - mocks required
-    @mock.patch('kg_idg.utils.sql_utils.process_data_dump')
+    @skip
+    @mock.patch('kg_idg.utils.sql_utils.process_data_dump', return_value=False)
     @mock.patch('koza.cli_runner.transform_source')
     def test_tcrd_transform(self, mock_process_data_dump, mock_transform_source):
-        t = TCRDTransform(self.input_dir,self.output_dir)
+        t = TCRDTransform(self.raw_path,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"tcrd")
         t.run()
         self.assertTrue(os.path.exists(this_output_dir))
@@ -118,14 +124,14 @@ class TestTransformUtils(TestCase):
         shutil.rmtree(this_output_dir)
 
     def test_hpa_transform(self):
-        t = ProteinAtlasTransform(self.input_dir,self.output_dir)
+        t = ProteinAtlasTransform(self.raw_path,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"hpa")
         t.run()
         self.assertTrue(os.path.exists(this_output_dir))
         shutil.rmtree(this_output_dir)
 
     def test_atc_transform(self):
-        t = ATCTransform(self.input_dir,self.output_dir)
+        t = ATCTransform(self.raw_path,self.output_dir)
         this_output_dir = os.path.join(self.output_dir,"atc")
         t.run()
         self.assertTrue(os.path.exists(this_output_dir))
