aa138ff28230eb04020867548a44d45e44131e04
caufieldjh <j.harry.caufield@gmail.com>
2021-12-23 13:28:52 -0500

    Revert "Minimal config for testing uPheno ETL"
    This reverts commit 3df4b918dea20a9af8495e38bd404992cd10294a.

--- kg_idg/transform.py
+++ kg_idg/transform.py
@@ -20,20 +20,20 @@ from kg_idg.transform_utils.ontology.ontology_transform import ONTOLOGIES
 
 
 DATA_SOURCES = {
-    # 'MondoTransform': OntologyTransform,
-    # 'ChebiTransform': OntologyTransform,
-    # 'HPOTransform': OntologyTransform,
-    # 'GOTransform': OntologyTransform,
-    # 'OGMSTransform': OntologyTransform,
-    # 'DrugCentralTransform': DrugCentralTransform,
-    # 'OrphanetTransform': OrphanetTransform,
-    # 'OMIMTransform': OMIMTransform,
-    # 'ReactomeTransform': ReactomeTransform,
-    # 'GOCAMTransform': GOCAMTransform,
-    # 'TCRDTransform': TCRDTransform,
-    # 'ProteinAtlasTransform': ProteinAtlasTransform,
-    # 'STRINGTransform': STRINGTransform,
-    # 'ATCTransform': ATCTransform,
+    'MondoTransform': OntologyTransform,
+    'ChebiTransform': OntologyTransform,
+    'HPOTransform': OntologyTransform,
+    'GOTransform': OntologyTransform,
+    'OGMSTransform': OntologyTransform,
+    'DrugCentralTransform': DrugCentralTransform,
+    'OrphanetTransform': OrphanetTransform,
+    'OMIMTransform': OMIMTransform,
+    'ReactomeTransform': ReactomeTransform,
+    'GOCAMTransform': GOCAMTransform,
+    'TCRDTransform': TCRDTransform,
+    'ProteinAtlasTransform': ProteinAtlasTransform,
+    'STRINGTransform': STRINGTransform,
+    'ATCTransform': ATCTransform,
     'UPhenoTransform': UPhenoTransform
 }
 
