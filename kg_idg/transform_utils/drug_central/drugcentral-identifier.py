45632cc333c95d67fe94da61331fa1b5b895c30b
caufieldjh <j.harry.caufield@gmail.com>
2022-01-06 13:29:39 -0500

    Ingest DrugCentral identifiers as attributes

--- kg_idg/transform_utils/drug_central/drugcentral-identifier.py
+++ kg_idg/transform_utils/drug_central/drugcentral-identifier.py
@@ -1,21 +1,22 @@
 import uuid
 
-from biolink_model_pydantic.model import Drug #type: ignore
+from biolink_model_pydantic.model import Association, Drug, NamedThing, Predicate #type: ignore
 
 from koza.cli_runner import koza_app #type: ignore
 
 # This parses all DrugCentral cross-references
-# Each row is a single xref to a single ID
-# so they are all combined upon merge
+# Each row could be a single xref to a single ID
+# but that isn't supported by BioLink yet
+# so we have a workaround with Association
 
 source_name="drugcentral-identifier"
 
 row = koza_app.get_row(source_name)
 
-xref = []
+# xref = []
 
 # the following is a map from id_type to
-# preferred prefix
+ # preferred prefix
 xref_types = {"CHEBI":"CHEBI",
                 "ChEMBL_ID":"CHEMBL_COMPOUND",
                 "DRUGBANK_ID":"DRUGBANK",
@@ -37,11 +38,28 @@ xref_types = {"CHEBI":"CHEBI",
                 "UNII":"UNII",
                 "VANDF":"VANDF",
                 "VUID":"VUID"}
-xref.append(xref_types[str(row["id_type"])] + ":" + str(row["identifier"]))
+# xref.append(xref_types[str(row["id_type"])] + ":" + str(row["identifier"]))
+
+# drug = Drug(id='DrugCentral:' + row["struct_id"],
+#             source='DrugCentral',
+#             xref=xref)
 
 # Entities
-drug = Drug(id='DrugCentral:' + row["struct_id"],
-            source='DrugCentral',
-            xref=xref)
+# CHEBI IDs already prefixed for some reason
+if str(row["id_type"]) == "CHEBI":
+    xref_curie = NamedThing(id=str(row["identifier"]))
+else:
+    xref_curie = NamedThing(id=xref_types[str(row["id_type"])] + ":" + str(row["identifier"]))
+drug = Drug(id='DrugCentral:' + row["struct_id"])
 
-koza_app.write(drug)
+# Association
+association = Association(
+    id="uuid:" + str(uuid.uuid1()),
+    subject=xref_curie.id,
+    predicate=Predicate.same_as,
+    object=drug.id,
+    relation="owl:sameAs",
+    source="DrugCentral"
+)
+
+koza_app.write(xref_curie, association, drug)
