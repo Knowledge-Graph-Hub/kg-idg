309294cf2eebc24b506f5d18237906bc8a691284
caufieldjh <j.harry.caufield@gmail.com>
2022-12-05 10:53:02 -0500

    Expand predicate types

--- kg_idg/transform_utils/drug_central/drugcentral-omop_relationship.py
+++ kg_idg/transform_utils/drug_central/drugcentral-omop_relationship.py
@@ -3,7 +3,7 @@ import uuid
 from biolink.model import Association, Disease, Drug, NamedThing  # type: ignore
 from koza.cli_runner import get_koza_app  # type: ignore
 
-'''
+"""
 This input contains links to a variety of
 different concepts, so they need to be
 mapped to more specific types based on their
@@ -11,7 +11,7 @@ presence in mappings (e.g., if a UMLS CUI
 has a mapping to an HP term, the association
 will involve the HP term) and their UMLS
 semantic type
-'''
+"""
 
 source_name = "drugcentral-omop_relationship"
 
@@ -19,17 +19,26 @@ koza_app = get_koza_app(source_name)
 row = koza_app.get_row()
 umls_to_mondo = koza_app.get_map("umls-cui_to_mondo_map")
 
+relation_to_predicate = {
+    "contraindication": "biolink:contraindicated_for",
+    "diagnosis": "biolink:diagnoses",
+    "indication": "biolink:treats",
+    "off-label use": "biolink:treats",
+    "reduce risk": "biolink:prevents",
+    "symptomatic treatment": "biolink:treats",
+}
+
 # Entities
 drug = Drug(
     id="DrugCentral:" + row["struct_id"],
     category="biolink:Drug",
 )
 
-# TODO: specify association type by relationship_name in table
 # TODO: ingest concepts besides diseases
 # TODO: add output to merge config
 
 umls_cui = "UMLS:" + row["umls_cui"]
+relationship_name = row["relationship_name"]
 
 if umls_cui in umls_to_mondo:
     mondo_id = umls_to_mondo[umls_cui]["mondo_id"]
@@ -43,11 +52,13 @@ else:
         category="biolink:NamedThing",
     )
 
+predicate = relation_to_predicate[relationship_name]
+
 # Association
 association = Association(
     id="uuid:" + str(uuid.uuid1()),
     subject=drug.id,
-    predicate="biolink:related_to",
+    predicate=predicate,
     object=concept.id,
     aggregator_knowledge_source="DrugCentral",
 )
